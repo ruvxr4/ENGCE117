a990a40dcca8199fd0189cbea15b103e2be1eaf9
jaylyn <apisitjf2549@gmail.com>
2025-11-21 17:24:21 +0700

    Update lab1.1.cpp

--- lab1/lab1.1.cpp
+++ lab1/lab1.1.cpp
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-
-int main() {
-
-    int N, i;
-    
-    printf("enter ur number:\n");
-
-    if (scanf("%d", &N) != 1) {
-        return 1; 
-    }
-    
-    for(i = 1; i <= N; i++){
+void print_hello_loop(int count) {
+    int i;
+    for (i = 0; i < count; i++) {
         printf("Hello Loop!\n");
     }
+}
 
-    
+int main() {
+    int input_number;
+
+    printf("Please enter a positive integer: ");
+
+    if (scanf("%d", &input_number) != 1) {
+        printf("Error: Invalid input format. Please enter a number.\n");
+        return 1; 
+    }
+
+    if (input_number < 0) {
+        printf("Warning: The number is negative. No output will be displayed.\n");
+    } else {
+        print_hello_loop(input_number);
+    }
 
     return 0;
 }
