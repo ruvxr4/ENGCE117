8646252b32184495a3c7efed813811edc70d5215
jaylyn <apisitjf2549@gmail.com>
2025-12-05 19:34:44 +0700

    Update rlab1.3.cpp

--- lab1/rlab1.3.cpp
+++ lab1/rlab1.3.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int *GetSet(int *num);
 
 int main() {
@@ -14,24 +13,21 @@ int main() {
 
 
 int *GetSet(int *num) {
-    
-    int *data; //สร้างมาเก็บก่อนreturn
+    int *data;
 
-    printf("how many num do u want? : "); 
+    printf("how many num do u want? : ");
     scanf("%d", num);
 
-   
     data = new int[*num]; 
 
     for (int i = 0; i < *num; i++) {
-        printf("Enter num %d: ", i + 1); 
-        scanf("%d", &data[i]); 
+        printf("Enter num %d: ", i + 1);
+        scanf("%d", &data[i]);
     }
 
-    printf("You entered:\n"); 
-    
+    printf("You entered:\n");
     for (int i = 0; i < *num; i++) {
-        printf("number %d: %d\n", i + 1, data[i]); 
+        printf("number %d: %d\n", i + 1, data[i]);
     }
     
     return data;
